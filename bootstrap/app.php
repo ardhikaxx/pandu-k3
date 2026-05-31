1e409ce3ffcc3c01833a28c300bdfc019d65ebc3
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-31 18:23:53 +0700

    chore: konfigurasi middleware dan alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+            'division.access' => \App\Http\Middleware\CheckDivisionAccess::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
